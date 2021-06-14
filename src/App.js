406a414d86fc29f2504070fa8c77eee6482ef993
msteele96 <marcsteele2014@gmail.com>
2021-06-14 11:50:13 -0400

    commit tree 40e5f499329af3c63198f4c9564f522c716353ae

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { Component } from "react";
+
+export default class App extends Component {
+    // constructor(props) {
+    //     super()
+        // this.state = {key:value}
+    // }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp => resp.json())
+        .then(json => this.setState({
+            people: json
+        }))
+    }
+
+    render() {
+        return <div>{this.state}</div>
+    }
+}
